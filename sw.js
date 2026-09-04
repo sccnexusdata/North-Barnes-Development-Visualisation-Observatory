667b3e73810df59b7b90ad49a9b34e4becdb6182
North Barnes Publisher <actions@users.noreply.github.com>
2026-09-04 04:47:06 +0000

    Publish approved visualisation outputs from research repo

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='north-barnes-observatory-v1';
+const CORE=['./','index.html','styles.css','app.js','data/phasing-model.json','assets/aerial-study.svg','assets/black-cap-study.svg','assets/icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put(event.request,copy)); return response;
+  }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('index.html'))));
+});
